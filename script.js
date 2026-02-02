1de5f404649f1e0e51778fe8ff2de6ac858aadbd
evvaall <evandr283@gmail.com>
2026-02-02 13:52:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Animação ao entrar na tela
 const reveals = document.querySelectorAll('.servico');
 
 const observer = new IntersectionObserver(entries => {
@@ -6,24 +7,43 @@ const observer = new IntersectionObserver(entries => {
       entry.target.classList.add('show');
     }
   });
-});
+}, { threshold: 0.2 });
 
 reveals.forEach(el => observer.observe(el));
-document.querySelector('img[src*="/assets/whatsapp"]').onclick = () =>{
-  window.open('https://wa.me/+244957847477.', '_blank');
+
+
+// WhatsApp
+const whatsappIcon = document.querySelector('img[src*="/assets/whatsapp"]');
+if (whatsappIcon) {
+  whatsappIcon.addEventListener('click', () => {
+    window.open('https://wa.me/244957847477', '_blank');
+  });
 }
 
-document.querySelectorAll('.contact').forEach(icon=>{
-  icon.addEventListener('click', ()=>{
-    const rede=icon.dataset.rede;
-    if (rede==='facebook'){
-      window.open('https://www.facebook.com/profile.php?id=61571858821505',  '_blank');
+
+// Redes sociais
+document.querySelectorAll('.contact').forEach(icon => {
+  icon.addEventListener('click', () => {
+    const rede = icon.dataset.rede;
+
+    if (rede === 'facebook') {
+      window.open(
+        'https://www.facebook.com/profile.php?id=61571858821505',
+        '_blank'
+      );
     }
-    if (rede==='instagram'){
-      window.open('https://www.instagram.com/#reactivated',  '_blank');
+
+    if (rede === 'instagram') {
+      window.open(
+        'https://www.instagram.com/#reactivated',
+        '_blank'
+      );
     }
   });
 });
-function ligar(){
-  window.location.href="tel:+244957847477"
+
+
+// Ligar
+function ligar() {
+  window.location.href = 'tel:+244957847477';
 }
